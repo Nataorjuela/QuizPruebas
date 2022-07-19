778ebf3b2ed53b35fa3693d63fed1a74115c580e
Nataorjuela <nataorjuela62@gmail.com>
2022-07-19 11:06:21 -0500

    corrección test WebLayer

--- src/test/java/co/edu/escuelaing/test/primerlive/TestingOnlyWebLayer.java
+++ src/test/java/co/edu/escuelaing/test/primerlive/TestingOnlyWebLayer.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+@WebMvcTest(HomeController.class)
 public class TestingOnlyWebLayer {
 
     @Autowired
